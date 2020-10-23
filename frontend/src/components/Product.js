ae10f528a4e092c02faf111d9eb2591eccbaf5ae
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-23 20:08:34 +0500

    typo fix

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <Link to={`/products/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
             </Link>
 
