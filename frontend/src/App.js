559c6387e0e1397a392abeb4286418fa3d6678d1
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-23 17:47:49 +0500

    Add component with simple bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <h1>Welcome to project</h1>
+        <Container>
+          <h1>Welcome to project</h1>
+        </Container>
       </main>
       <Footer />
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <header>
-            header
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Navbar.Brand href="/">TestShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/login">Sign-in</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 }
