30dcf1a2870545a12b32ff89615623706d46fc39
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-24 19:32:48 +0500

    add productlist reducer

---
+++ frontend/src/reducers/productReducer.js
@@ -0,0 +1,12 @@
+export const productListReducer = (state = { products: [] }, { type, payload }) => {
+    switch (type) {
+        case 'PRODUCT_LIST_REQUEST':
+            return { loading: true, products: [] }
+        case 'PRODUCT_LIST_SUCCESS':
+            return { loading: false, products: payload }
+        case 'PRODUCT_LIST_FAIL':
+            return { loading: false, error: payload }
+        default:
+            return state
+    }
+}
