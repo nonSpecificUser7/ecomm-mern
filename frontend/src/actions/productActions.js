0092a665ea8b1130297089a7f3b7c86ca5a6598d
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-24 21:25:17 +0500

    add connection to Redux

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,11 @@
-import { PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST } from '../constants/productConstants'
 import axios from 'axios'
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+} from '../constants/productConstants'
 
-export const listProducts = (payload) => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
@@ -9,14 +13,15 @@ export const listProducts = (payload) => async (dispatch) => {
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
-            payload: data
+            payload: data,
         })
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
         })
     }
 }
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,13 +1,17 @@
-import { PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST } from '../constants/productConstants'
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+} from '../constants/productConstants'
 
-export const productListReducer = (state = { products: [] }, { type, payload }) => {
-    switch (type) {
-        case PRODUCT_LIST_FAIL:
+export const productListReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case PRODUCT_LIST_REQUEST:
             return { loading: true, products: [] }
         case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: payload }
-        case PRODUCT_LIST_REQUEST:
-            return { loading: false, error: payload }
+            return { loading: false, products: action.payload }
+        case PRODUCT_LIST_FAIL:
+            return { loading: false, error: action.payload }
         default:
             return state
     }
