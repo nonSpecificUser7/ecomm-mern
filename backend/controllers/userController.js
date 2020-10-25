9a6dcd2508ce95a80081a46817d6c11cdc774fa7
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 15:15:46 +0500

    add user registration route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,37 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(401).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid User Data")
+    }
+
+})
+
 const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
@@ -37,4 +68,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { authUser, getUserProfile, registerUser }
