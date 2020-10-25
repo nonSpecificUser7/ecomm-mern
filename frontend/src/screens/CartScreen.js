a1b516c0c1493ee8820845597dfd925a0e7c7b74
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 08:57:18 +0500

    add Cart Screen functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Message from '../components/Message'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+
+    const productId = match.params.id
+
+    const quantity = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, quantity))
+        }
+    }, [dispatch, productId, quantity])
+
     return (
         <div>
             Cart
