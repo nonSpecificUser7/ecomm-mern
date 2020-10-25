943688e58ef3e6709ac9e3f6f93c761f70fb41cd
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 15:41:25 +0500

    add local storage implementation for user info

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,24 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productDetailsReducer, productListReducer } from './reducers/productReducer'
 import { cartReducer } from './reducers/cartReducer'
+import { userLoginReducer } from './reducers/userReducer'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 const storageCartItems = localStorage.getItem('cartItems') ? JSON.parse
     (localStorage.getItem('cartItems')) : []
 
+const storageUserInfo = localStorage.getItem('userInfo') ? JSON.parse
+    (localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: storageCartItems }
+    cart: { cartItems: storageCartItems },
+    userLogin: { userInfo: storageUserInfo }
 }
 
 const middleware = [thunk]
