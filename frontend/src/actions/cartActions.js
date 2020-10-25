a968bdbc333c99d3b0d579c097b1dd12c63bce13
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 08:54:46 +0500

    bug fix

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 import {
-    CARD_ADD_PRODUCT,
-    CARD_REMOVE_PRODUCT,
+    CART_ADD_PRODUCT,
+    CART_REMOVE_PRODUCT,
 } from '../constants/cartConstants'
 
 export const addToCart = (id, quantity) => async (dispatch, getState) => {
-    const { data } = await axios.get(`api/products/${id}`)
+    const { data } = await axios.get(`/api/products/${id}`)
 
     dispatch({
-        type: CARD_ADD_PRODUCT,
+        type: CART_ADD_PRODUCT,
         payload: {
             product: data._id,
             name: data.name,
             image: data.image,
             price: data.price,
             countInStock: data.countInStock,
-            quantity
-        }
+            quantity,
+        },
     })
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
