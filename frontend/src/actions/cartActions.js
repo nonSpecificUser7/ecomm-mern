17d9bfbb33c602a1b5991364750016121557bb61
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 18:27:31 +0500

    add save shipping address action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import {
     CART_ADD_PRODUCT,
     CART_REMOVE_PRODUCT,
+    CART_SAVE_SHIPPING_ADDRESS
 } from '../constants/cartConstants'
 
 export const addToCart = (id, quantity) => async (dispatch, getState) => {
@@ -29,4 +30,13 @@ export const removeFromCart = (id) => (dispatch, getState) => {
     })
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data,
+    })
+
+    localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
