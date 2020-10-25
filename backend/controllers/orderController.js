4bf46be649b20165433417c99c69c30358c8ed26
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 21:57:22 +0500

    typo fix

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
-// @desc    Create new order
-// @route   POST /api/orders
-// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
     const {
         orderItems,
@@ -18,7 +15,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
     if (orderItems && orderItems.length === 0) {
         res.status(400)
         throw new Error('No order items')
-        return
     } else {
         const order = new Order({
             orderItems,
