2405f1cc5fcd4ae365113547e754f8bbb23a59af
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-25 08:10:29 +0500

    add cart reducer

---
+++ frontend/src/reducers/cartReducer.js
@@ -0,0 +1,30 @@
+import {
+    CARD_ADD_PRODUCT,
+    CARD_REMOVE_PRODUCT,
+} from '../constants/cartConstants'
+
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type) {
+
+        case CARD_ADD_PRODUCT:
+            const item = action.payload
+            const isExistItem = state.cartItems.find(v => v.product === item.product)
+
+            if (isExistItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(v => v.product === isExistItem
+                        .product ? item : v)
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+
+        default:
+            return state
+    }
+}
