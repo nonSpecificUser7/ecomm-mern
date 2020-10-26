50c0ac4c66b4b328540061c95cfbc9b09b21b6d7
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-26 08:41:36 +0500

    add deleteUser route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,7 +19,16 @@ const getProduct = asyncHandler(async (req, res) => {
 
 })
 
-export {
-    getProduct,
-    getProducts
-}
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export { getProducts, getProduct, deleteProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import {
     getProduct,
-    getProducts
+    getProducts,
+    deleteProduct,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProduct)
+router.route('/:id').get(getProduct).delete(protect, admin, deleteProduct)
 
 export default router
