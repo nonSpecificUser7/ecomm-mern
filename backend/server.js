da82fc38b1453328f4714cf7b7546751a89d3661
nonSpecificUser7 <gsirazh@gmail.com>
2020-10-26 09:18:01 +0500

    add file upload routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import orderRoutes from './routes/orderRoutes.js'
+import path from 'path'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -21,10 +23,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
